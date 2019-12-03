cdc6f63739645adb9c1577f119a8b70c102a4815
TrevorHD <44789861+TrevorHD@users.noreply.github.com>
2019-12-03 10:24:15 -0500

    Clean-up
    Cleaned up several parts of the code. This involved changing numbering of the different models, removing some redundant code, and adding a visualisation of the correlation matrix.

--- PhysicalAnalysis.R
+++ PhysicalAnalysis.R
@@ -4,6 +4,7 @@
 library(tidyverse)
 library(leaps)
 library(car)
+library(corrplot)
 
 # Initialise data
 data <- read.csv("https://raw.githubusercontent.com/TrevorHD/CNSeedDrop/master/SeedDropData.csv")
@@ -17,7 +18,10 @@ data %>% mutate(SeedAngle = acos(1 - ((SeedWidth^2)/(2*(SeedLength^2)))),
                 TV = 1.25/DT.Avg) -> data
 
 # Create new data set that excludes instances where pappus length, width, or DT are NA
-data.new <- data[complete.cases(data[ , 23:25]), ]
+data.new <- data[complete.cases(data[, 23:25]), ]
+
+# Remove any rows where the quantities defined above are NaN
+data.new <- data.new[-which(is.nan(data.new[, 36])), ]
 
 
 
@@ -28,14 +32,15 @@ data.new <- data[complete.cases(data[ , 23:25]), ]
 # Are there correlations between terms? If so, multicollinearity may be a problem.
 pairs(data.new[, c("TV", "SeedLength", "SeedWidth", "SeedAngle", "SeedArea", "SeedHeight", "SeedVol")])
 cor(data.new[, c("TV", "SeedLength", "SeedWidth", "SeedAngle", "SeedArea", "SeedHeight", "SeedVol")])
+corrplot(cor(data.new[, c("TV", "SeedLength", "SeedWidth", "SeedAngle", "SeedArea", "SeedHeight", "SeedVol")]),
+         method = "square", type = "upper")
 
 # Looks like TV may be influenced by SeedWidth, SeedAngle, SeedArea, and SeedVol
-# There will likely be a high degree of multicollinearity
-# Note: NAs in correlation matrix because SeedAngle has some values that are NaN
+# There will likely be a high degree of multicollinearity as well
 
 # Generate full model with all predictors
-mod.full <- lm(TV ~ SeedWidth + SeedLength + SeedWidth:SeedLength + SeedArea + SeedAngle +
-                    SeedHeight + SeedVol, data = data.new)
+mod.full <- lm(TV ~ SeedWidth + SeedLength + SeedArea + SeedAngle + SeedHeight + 
+                    SeedVol, data = data.new)
 summary(mod.full)
 
 # VIF is overwhelmingly high
@@ -48,48 +53,42 @@ vif(mod.full)
 ##### Variable selection ----------------------------------------------------------------------------------
 
 # Backward elimination, no AIC or BIC; remove "worst" predictor
-mod.1 <- update(mod.full, . ~ . - SeedWidth:SeedLength)
-summary(mod.1)
-mod.1 <- update(mod.1, . ~ . - SeedAngle)
+mod.1 <- update(mod.full, . ~ . - SeedAngle)
 summary(mod.1)
 mod.1 <- update(mod.1, . ~ . - SeedArea)
 summary(mod.1)
 
-# Backward elimination with AIC
+# Backward elimination with AIC produces the exact same result
 step(mod.full, direction = "backward", data = data.new)
-mod.2 <- lm(TV ~ SeedWidth + SeedLength + SeedHeight + SeedVol, data = data.new)
-summary(mod.2)
-mod.2 <- update(mod.2, . ~ . - SeedLength)
-summary(mod.2)
+
+# Step in both directions with AIC produces the exact same result
+step(mod.full, direction = "both", data = data.new)
 
 # Forward selection with AIC
 mod.null <- lm(TV ~ 1, data = data.new)
-mod.3 <- lm(TV ~ SeedWidth + SeedLength + SeedWidth:SeedLength + SeedArea + SeedAngle +
-                 SeedHeight + SeedVol, data = data.new)
-step(mod.null, direction = "forward", scope = list(lower = mod.null, upper = mod.3), data = data.new)
-mod.3 <- lm(TV ~ SeedWidth + SeedArea, data = data.new)
-summary(mod.3)
+step(mod.null, direction = "forward", scope = list(lower = mod.null, upper = mod.full), data = data.new)
+mod.2 <- lm(TV ~ SeedWidth + SeedArea, data = data.new)
+summary(mod.2)
 
-# Models 1 and 2 are the same, but all predictors have high VIF
+# All predictors in model 1 have a high VIF; same for model 2, but not as bad
 vif(mod.1)
 vif(mod.2)
 
-# Model 3 also has high VIF; makes sense several predictors are calculated using other predictors
-vif(mod.3)
+# Model 2 has an R^2 that's only slightly lower than that of model 1
+# Overall, model 2 seems like the best choice because of lower VIF
+# We should trade a large decrease in VIF for only a small decrease in R^2
 
-# mod.3 has an R^2 that's only slightly lower than the other two models
-# Overall, mod.3 seems like the best choice because of lower overall VIF
 # VIF is still high, but the SeedArea term is significant when added; partial F-test confirms this
-mod.4 <- lm(TV ~ SeedWidth, data = data.new)
-anova(mod.4, mod.3)
+mod.3 <- lm(TV ~ SeedWidth, data = data.new)
+anova(mod.2, mod.3)
 
 # If we want to keep only two variables, SeedWidth and SeedArea are indeed the ones we want
-summary(regsubsets(TV ~ SeedWidth + SeedLength + SeedWidth:SeedLength + SeedArea + SeedAngle +
-                        SeedHeight + SeedVol, data = data.new))
+summary(regsubsets(TV ~ SeedWidth + SeedLength + SeedArea + SeedAngle + SeedHeight + 
+                        SeedVol, data = data.new))
 
 # However, SeedWidth and SeedArea contribute to high VIF, so we will want to remove one
 # Given that SeedArea ~ Seedwidth^2, then we can just express it as such and not worry about VIF
-mod.5 <- lm(TV ~ SeedWidth + I(SeedWidth^2), data = data.new)
+mod.4 <- lm(TV ~ SeedWidth + I(SeedWidth^2), data = data.new)
 
 # The model with just the linear term might not be enough since its R^2 is significantly lower
 summary(mod.3)
@@ -98,12 +97,12 @@ summary(mod.4)
 # Also, the fit of the model with only linear SeedWidth isn't as good:
 plot(TV ~ SeedWidth, data = data.new)
 testdata <- data.frame(SeedWidth = seq(min(data.new$SeedWidth), max(data.new$SeedWidth), length.out = 300))
-testdata$TV = predict(mod.5, newdata = newdat)
-with(testdata, lines(x = SeedWidth, y = TV, col = "red"))
+testdata$TV = predict(mod.4, newdata = testdata)
+with(testdata, lines(x = SeedWidth, y = TV, col = "chocolate1", lwd = 2))
+abline(mod.3, col = "deepskyblue", lwd = 2)
 remove(testdata)
-abline(mod.4, col = "blue")
 
-# Thus, mod.5 (SeedWidth and SeedWidth^2) will be the best choice
+# Thus, model 3 (SeedWidth and SeedWidth^2) will be the best choice
 
 
 
@@ -111,19 +110,27 @@ abline(mod.4, col = "blue")
 
 ##### Model diagnostics -----------------------------------------------------------------------------------
 
-# Model seems to be a decent fit
+# Model 3 seems to be a decent fit
 plot(TV ~ SeedWidth, data = data.new)
 testdata <- data.frame(SeedWidth = seq(min(data.new$SeedWidth), max(data.new$SeedWidth), length.out = 300))
-testdata$TV = predict(mod.5, newdata = newdat)
+testdata$TV = predict(mod.4, newdata = newdat)
 with(testdata, lines(x = SeedWidth, y = TV, col = "red"))
-remove(testdata)
 
 # No discernable pattern in standardised residuals
-plot(rstandard(mod.5) ~ SeedWidth, data = data.new)
+plot(rstandard(mod.4) ~ SeedWidth, data = data.new, ylab = "Standardised Residuals")
 abline(h = 0, col = "red")
 abline(h = c(-2, 2), col = "red", lty = 3)
 
 # Other diagnostic plots seem fine
 # Only a few outliers and bad leverage points, given the large amount of data
 # Residuals are mostly normally distributed, with only a bit of departure at the tails
-plot(mod.5)
+plot(mod.4)
+
+# Curved patterns in standardised residuals for model 4
+# We made the right choice by adding the quadratic term
+plot(rstandard(mod.3) ~ SeedWidth, data = data.new, ylab = "Standardised Residuals")
+abline(h = 0, col = "red")
+abline(h = c(-2, 2), col = "red", lty = 3)
+
+# More diagnostics for model 4
+plot(mod.3)
